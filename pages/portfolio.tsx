3a99b23140b714979ee1278ee78dda80aff64886
nquanghien97 <nquanghien97@gmail.com>
2022-04-10 18:29:11 +0700

    overlay

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -9,6 +9,7 @@ import Image from 'next/image';
 import adidas from '../Assets/adidas.png'
 import music from '../Assets/music.png'
 import tesla from '../Assets/tesla.png'
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
 export interface  MyPortfolioProps {
 }
@@ -17,10 +18,10 @@ export default function MyPortfolio (props: MyPortfolioProps) {
   return (
     <div className="portfolio flex justify-center mx-auto w-[100vw]">
       <div className="text-black w-5/6 flex flex-col justify-center items-center">
-        <h1 className="font-medium leading-tight text-5xl my-4">My Portfolio</h1>
+        <h1 className="font-medium leading-tight text-5xl my-4">My Project</h1>
         <p>I love what I do. I take great pride in what I do.</p>
         <div className="slide w-20 h-0.5 bg-slate-400 my-6"></div>
-        <div className="w-full h-full relative">
+        <div className="w-full h-full">
           <Swiper
             modules={[Navigation, Pagination, Scrollbar]}
             slidesPerView={1}
@@ -31,33 +32,60 @@ export default function MyPortfolio (props: MyPortfolioProps) {
             // scrollbar={{ draggable: true }}
           >
             <SwiperSlide>
-              <div className="w-full h-[30vh] md:h-[100vh]">
+              <div className="image w-full h-[30vh] md:h-[100vh] relative">
                 <Image
                   src={adidas} 
                   layout="fill"
-                  className="" 
-                  alt="Logo"
+                  className="-z-10" 
+                  alt="Adidas"
                 />
+                <div className="overlay absolute inset-0 bg-gray-300/[0.7] flex justify-center items-center hidden">
+                  <a 
+                    href="https://hien-adidasclone.netlify.app/"
+                    className="absolute px-8 py-4 bg-sky-800 hover:bg-amber-500 rounded text-white"
+                  >
+                    Visit now
+                    <ArrowRightAltIcon className="ml-2"/>
+                  </a>
+                </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="w-full h-[30vh] md:h-[100vh]">
+              <div className="image w-full h-[30vh] md:h-[100vh] relative">
                 <Image
                   src={music}
                   layout="fill"
-                  className="" 
-                  alt="Logo"
+                  className="-z-10" 
+                  alt="MusicPlayer"
                 />
+                <div className="overlay absolute inset-0 bg-gray-300/[0.7] flex justify-center items-center hidden">
+                  <a 
+                    href="https://nquanghien97.github.io/MusicPlayer/"
+                    className="absolute top-1/2 right-1/2 translate-x-2/4 px-8 py-4 bg-sky-800 hover:bg-amber-500 rounded text-white"
+                  >
+                    Visit now
+                    <ArrowRightAltIcon className="ml-2"/>
+                  </a>
+                </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="w-full h-[30vh] md:h-[100vh]">
+              <div className="image w-full h-[30vh] md:h-[100vh] relative">
                 <Image
                   src={tesla} 
                   layout="fill"
-                  className="" 
-                  alt="Logo"
+                  className="-z-10" 
+                  alt="Tesla"
                 />
+                <div className="overlay absolute inset-0 bg-gray-300/[0.7] flex justify-center items-center hidden">
+                  <a 
+                    href="https://hien-teslaclone.netlify.app/"
+                    className="absolute top-1/2 right-1/2 translate-x-2/4 px-8 py-4 bg-sky-800 hover:bg-amber-500 rounded text-white"
+                  >
+                    Visit now
+                    <ArrowRightAltIcon className="ml-2"/>
+                  </a>
+                </div>
               </div>
             </SwiperSlide>
           </Swiper>
